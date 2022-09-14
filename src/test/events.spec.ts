28994974c871e0c4d635d9e2c005ca41cb0b8acd
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-09-14 15:47:13 +0300

    update test

--- src/test/events.spec.ts
+++ src/test/events.spec.ts
@@ -3,18 +3,44 @@ import {AsyncSeriesEventEmitter} from '../events';
 describe('AsyncSeriesEventEmitter', () => {
 
     it('should use AsyncSeriesEventEmitter.subscribe()', async ()=> {
-        const emitter = new AsyncSeriesEventEmitter<{value: number}>();
-        emitter.subscribe(async (event: { value: number }) => {
+        class MyClass {
+            public readonly load = new AsyncSeriesEventEmitter<{value: number}>();
+        }
+        const item = new MyClass();
+        item.load.subscribe(async (event: { value: number }) => {
             event.value += 1;
         });
-        emitter.subscribe(async (event: { value: number }) => {
+        item.load.subscribe(async (event: { value: number }) => {
             event.value += 1;
         });
         const eventArgs = {
             value: 100
         }
-        await emitter.emit(eventArgs);
+        await item.load.emit(eventArgs);
         expect(eventArgs.value).toBe(102);
+    });
+
+    it('should use AsyncSeriesEventEmitter.subscribe() with timers', async ()=> {
+        class MyClass {
+            public readonly load = new AsyncSeriesEventEmitter<{status: string}>();
+        }
+        const item = new MyClass();
+        item.load.subscribe(async (event: { status: string }) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    event.status = 'active';
+                    resolve();
+                }, 1000);
+            });
+        });
+        item.load.subscribe(async (event: { status: string }) => {
+            event.status = 'unknown';
+        });
+        const eventArgs = {
+            status: null
+        }
+        await item.load.emit(eventArgs);
+        expect(eventArgs.status).toBe('unknown');
     });
 
 });
