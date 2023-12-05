e3a7eb8a68937b7847691e5b9150e86c821b38a1
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-12-05 07:26:17 +0000

    update rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -4,26 +4,35 @@ const path = require("path");
 
 module.exports = [
   {
-    input: "src/index.ts",
-    output: [
-      {
-        file: `dist/index.cjs.js`,
-        format: "cjs",
-      },
-      {
-        file: `dist/index.esm.js`,
-        format: "esm",
-      },
-      {
-        name: "@themost/client",
-        file: `dist/index.js`,
-        format: "umd",
-      },
-    ],
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist',
+      format: 'cjs',
+      sourcemap: true
+    },
     external: Object.keys(pkg.dependencies),
-    plugins: [typescript({
-      tsconfig: path.resolve(process.cwd(), "tsconfig.lib.json")
-    })],
+    plugins: [typescript()]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    external: Object.keys(pkg.dependencies),
+    plugins: [typescript()]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      name: '@themost/client',
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      sourcemap: true
+    },
+    external: Object.keys(pkg.dependencies),
+    plugins: [typescript()]
   },
   {
     input: path.resolve(process.cwd(), 'common/src/index.ts'),
